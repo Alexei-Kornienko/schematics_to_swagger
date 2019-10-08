677e1eeadee148402e99637cbe74f1e8c85e6e88
Alexei Kornienko <alexei.kornienko@raccoongang.com>
2019-10-08 18:38:14 +0300

    Bump minor version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ with open(join(dirname(__file__), 'README.md')) as f:
 
 setup(
     name='schematics-to-swagger',
-    version='1.2.1',
+    version='1.2.2',
     install_requires=required,
     url='https://github.com/Alexei-Kornienko/schematics_to_swagger',
     license='MIT',
